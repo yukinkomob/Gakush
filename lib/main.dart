75a95f25b34a5df1abc292524c2480202929bd33
Yuki Nishimura <yukinkopen@gmail.com>
2025-07-05 13:03:58 +0900

    レーダーチャート画面を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -76,6 +77,7 @@ class _MainScreenState extends State<MainScreen> {
           HomeScreen(),
           SearchScreen(),
           AccountScreen(),
+          FoodRadarChartScreen()
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -95,6 +97,10 @@ class _MainScreenState extends State<MainScreen> {
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
             label: 'Account',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.radar), // レーダーチャート用のアイコン
+            label: 'Food',
           ),
         ],
       ),
@@ -241,6 +247,61 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class FoodRadarChartScreen extends StatelessWidget {
+  const FoodRadarChartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('折れ線グラフ'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          width: screenWidth * 0.95,
+          height: screenWidth * 0.95 * 0.65,
+          child: LineChart(
+            LineChartData(
+              lineBarsData: [
+                LineChartBarData(
+                  spots: const [
+                    FlSpot(1, 0),
+                    FlSpot(2, 400),
+                    FlSpot(3, 650),
+                    FlSpot(4, 800),
+                    FlSpot(5, 870),
+                    FlSpot(6, 920),
+                    FlSpot(7, 960),
+                    FlSpot(8, 980),
+                    FlSpot(9, 990),
+                    FlSpot(10, 995),
+                  ],
+                  isCurved: true,
+                  color: Colors.blue,
+                ),
+              ],
+              titlesData: const FlTitlesData(
+                topTitles: AxisTitles(
+                  axisNameWidget: Text(
+                    "労働の限界生産性",
+                  ),
+                  axisNameSize: 35.0,
+                ),
+                rightTitles:
+                    AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              ),
+              maxY: 1000,
+              minY: 0,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
